457f75e1b0ac6f749c293f424f56c0ac9dfa801f
Irena Karpan <irenakarpan@gmail.com>
2026-06-20 16:50:59 +0200

    do task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+'use strict'
 
+const loginForm = document.querySelector('.login-form');
+const formObj = {};
+
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    formObj.email = form.elements.email.value.trim();
+    formObj.password = form.elements.password.value.trim();
+
+    if (formObj.email === '' || formObj.password === '') {
+        alert('All form fields must be filled in');
+    }
+
+    console.log(formObj);
+    form.reset();
+
+    
+});

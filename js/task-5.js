5e75c9e2f092ed2c90322a08b6590fc4ed6509f7
Irena Karpan <irenakarpan@gmail.com>
2026-06-20 17:00:00 +0200

    do task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,19 @@
+'use string'
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const elemBody = document.querySelector('body');
+const spanValue = document.querySelector('.color');
+
+elemBody.addEventListener('click', event => {
+  const colorValue = getRandomHexColor();
+  
+  elemBody.style.backgroundColor = colorValue;
+  spanValue.textContent = colorValue;
+
+} )
